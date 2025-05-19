f60dad0521f2b38d1a57b3052a4324e1fa38c33c
greg <greg.jwaerhouse@gmail.com>
2025-05-18 20:04:51 -0400

    Afyonlte-common: Move build_fingerprint to afyonltecan. Makes sense if we ever need to make separate build per device.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,8 +20,6 @@ COMMON_PATH := device/samsung/afyonlte-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
 
-BUILD_FINGERPRINT := samsung/afyonltevl/afyonltecan:4.4.2/KOT49H/G386WVLS1AQB1:user/release-keys
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := afyonlte,afyonltetmo,afyonltecan
 
