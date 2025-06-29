1680d1478dac8cb642f9b49361577cfe5cac85a2
greg <greg.jwaerhouse@gmail.com>
2025-06-29 06:47:24 -0400

    Afyonlte: Use MIT Open source libaptX for libaptX_encoder and libaptXHD_encoder.

--- afyonlte.mk
+++ afyonlte.mk
@@ -73,7 +73,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 
-# Open source libaptX - https://github.com/notmyst33d/libaptXAdd commentMore actions
+# Open source libaptX - https://github.com/notmyst33d/libaptX
 PRODUCT_PACKAGES += \
     libaptX_encoder \
     libaptXHD_encoder
